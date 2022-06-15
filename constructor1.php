8300008dd322b9bd4515543b96675f2aac1ebdce
Mofasser <mofasserrakib@gmail.com>
2022-06-15 13:35:17 +0600

    private property public function example and Initialized property with constructor

---
+++ constructor1.php
@@ -0,0 +1,30 @@
+<?php
+/* How to access private property with public method And
+ property initialized with  constructor */
+
+class customer {
+
+private $id;
+private $name;
+private $email;
+
+public function __construct($id=0,$name="No name",$email="No email")
+{
+    $this->id=$id;
+    $this->name=$name;
+    $this->email=$email;
+    
+}
+public function getInfo() {
+   echo 'ID='.' '.$this->id.' '.'Name='.' '.$this->name.' '.'Email='.' '.$this->email."<br>";
+}
+
+}
+
+$obj1= new customer(1,"Rakib","Rakib@gmail.com");
+$obj1->getInfo();
+$obj1= new customer(1,"Sakib",);
+$obj1->getInfo();
+
+
+?>
