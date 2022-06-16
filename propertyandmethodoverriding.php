3c060596cc0660514bca36d54547ced2ece23bca
Mofasser <mofasserrakib@gmail.com>
2022-06-17 01:03:16 +0600

    property and method overriding example

---
+++ propertyandmethodoverriding.php
@@ -0,0 +1,35 @@
+<?php
+/*   Property overriding and Method overriding */
+
+use customer as GlobalCustomer;
+use subscriber as GlobalSubscriber;
+
+class customer {
+    public $name="Hasib from customer section";
+    public function show()
+    {
+        return $this->name;
+    }
+}
+class subscriber extends customer{
+    public $name="Halim from subscriber section";
+    public function show()
+    {
+        return  $this->name;
+    }
+}
+$obj1= new customer();
+echo $obj1->show()."<br>";
+$obj2= new subscriber();
+echo $obj2->show()."<br>";
+/*        
+output
+--------------------------------------------------------------
+Hasib from customer section
+Halim from subscriber section
+
+*/
+
+
+
+?>
