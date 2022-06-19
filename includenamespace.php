51ab29f1605057ab49474b993cd647de3ebd0abc
Mofasser <mofasserrakib@gmail.com>
2022-06-19 20:04:16 +0600

    namespace file modified

--- includenamespace.php
+++ includenamespace.php
@@ -1,12 +1,19 @@
 <?php
 require 'namespace.php';
 require 'namespace1.php';
-
-$obj2=new \two\Hello();
+/* import namespace follow below code
+use two\Hello;
+$obj2=new Hello();
+*/
+/* without importing namespace follow below code. 
+Remember Fully qualified names required from namespace */
+$obj2=new two\Hello(); 
 $obj2->greeting();
+/*output 
+Good morning! This message is from namepace one,class Hello
+H! This message is from namepace two,class Hello
 
-
-
+*/
 
 
 ?>
